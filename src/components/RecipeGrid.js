2ec0dc93d3b6aeed329bf53fcf4ab45d9f3a31b0
Seth Poulin <seth.poulin@gmail.com>
2019-09-12 21:57:49 -0400

    Got IDs printing to page

--- src/components/RecipeGrid.js
+++ src/components/RecipeGrid.js
@@ -2,14 +2,94 @@ import React,{Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import axios from 'axios';
 
+// let recipeArray = ['test','anothertest']
+
 class RecipeGrid extends Component {
+    constructor(){
+        super();
+        this.state = {
+            userCategory: '',
+            userRecipes: []
+        }
+    }
+
+    updateUserCategory = (e) => {
+
+        this.setState({
+            userCategory: e.target.value
+        }, 
+        () => {
+            this.axiosCall(this.state.userCategory)
+        })
+    }
+
+
+
+    axiosCall = (updatedCategory) => {
+        
+        axios({
+            url:`https://www.themealdb.com/api/json/v1/1/filter.php?c=${updatedCategory}`,
+            method: 'GET',
+            dataResponse: 'json'
+        }).then((response) => {
+            // recipeArray.length = 0;
+            return response
+        })
+        .then((response) => {
+            const recipeArray = [];
+
+            recipeArray.push(response.data.meals);
+
+
+            this.setState({
+                userRecipes: recipeArray[0]
+            })
+
+         
+            console.log(this.state.userRecipes)
+           console.log(recipeArray)
+        })
+            
+        
+    }
+
+    // getRecipes = () => {
+        
+
+        
+    // }
+
   
     render(){
-    
+
         return (
         
             <div className="">
-                <h1>This is the recipe grid page</h1>
+                <h1>Find a recipe</h1>
+                <select onChange={this.updateUserCategory} name="" id="">  
+                    <option value="Beef">Beef</option>
+                    <option value="Chicken">Chicken</option>
+                    <option value="Dessert">Dessert</option>
+                    <option value="Lamb">Lamb</option>
+                    <option value="Pasta">Pasta</option>
+                    <option value="Pork">Pork</option>
+                    <option value="Seafood">Seafood</option>
+                    <option value="Side">Side</option>
+                    <option value="Starter">Starter</option>
+                    <option value="Vegan">Vegan</option>
+                    <option value="Vegetarian">Vegetarian</option>
+                    <option value="Miscellaneous">Miscellaneous</option>
+                </select>
+
+                {/* {console.log(this.state.userRecipes)} */}
+                {/* {console.log('cat')} */}
+ 
+                {this.state.userRecipes.map((recipe, i) => {
+                    console.log(recipe) 
+                   return <h1 key={recipe.idMeal}>{recipe.idMeal}</h1>
+                })}
+
+
                 <Link to="/">Home</Link>
                 <Link to="/event/:">Event Page</Link>
                 <Link to="/fullrecipe/:">Full Recipe</Link>
