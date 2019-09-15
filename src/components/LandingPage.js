1015278fb710f416172fa497e292cd4c9181c3bd
Aaron <aaronjunehee@gmail.com>
2019-09-15 19:44:30 -0400

    styling stuff

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -21,12 +21,15 @@ const LandingPage = (props) => {
             </header>
             <section className="start">
                 <div className="wrapper startContainer">
+                    <div className="tagLine">
+                        <h2>We help you organize Dinner Parties</h2>
+                    </div>
                     <div className="startForm">
                         <form 
                         // onSubmit={this.pushToFirebase} 
                         onSubmit={props.createEvent}
                         action="">
-                            <input onChange={props.getEventName} name="createEvent" className="createEvent" type="text"/>
+                            <input onChange={props.getEventName} name="createEvent" className="createEvent" type="text" placeholder="enter your group name"/>
                             <button className="submit">
                                 Submit
                                 {/* This button creates a new event object in the events array AND links to event page. */}
@@ -41,18 +44,27 @@ const LandingPage = (props) => {
                 </div>
             </section>
             <section className="events">
-                {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
-                {/* {props["event"] ?console.log(props["event"][0]):console.log('null')} */}
-                {props.event?
-                    props.event.map((userEvents, eventIndex) => {
-                        return (
-                            // console.log(userEvents.eventName)
-                            <Link to={`/dashboard/${userEvents.eventName}`} 
-                            key={eventIndex}>{userEvents.eventName}</Link>
-                        )
-                    }
-                ):null}
-
+                <div className="wrapper">
+                    <div>
+                        <h2>Events</h2>
+                        <h3>Find your dinner party</h3>
+                    </div>
+                    <ul>
+                        {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
+                        {/* {props["event"] ?console.log(props["event"][0]):console.log('null')} */}
+                        {props.event?
+                            props.event.map((userEvents, eventIndex) => {
+                                return (
+                                    // console.log(userEvents.eventName)
+                                    <li>
+                                        <Link to={`/dashboard/${userEvents.eventName}`} 
+                                        key={eventIndex}>{userEvents.eventName}</Link>
+                                    </li>
+                                )
+                            }
+                        ):null}
+                    </ul>
+                </div>
             </section>
         </div>
     
