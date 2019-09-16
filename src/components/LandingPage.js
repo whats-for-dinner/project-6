2c9d311e8506f27656777cc162cb038e7a337392
Aaron <aaronjunehee@gmail.com>
2019-09-16 12:44:34 -0400

    landing page responsive and recipe grid styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,75 +1,86 @@
 import React,{Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import heroImage from '../images/heroImage.png'
 import firebase from '../firebase';
+import {Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 
 
-const LandingPage = (props) => {
+class LandingPage extends Component {
+
+    moveToSection = () => {
+        scroller.scrollTo('events', {
+            duration: 1500,
+            delay: 20,
+            smooth: "easeInOutQuint",
+        });
+    }
+
   
-    return (
-    
-        <div className="landingPage">
-            <header className="landingHeader">
-                <h1>What's For Dinner?</h1>
-                {/* <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/event/:">Event Page</Link></li>
-                    </ul>
-                </nav> */}
-            </header>
-            <section className="start">
-                <div className="wrapper startContainer">
-                    <div className="tagLine">
-                        <h2>We help you organize Dinner Parties</h2>
+    render(){
+        return (
+            <div className="landingPage">
+                <header className="landingHeader">
+                    <h1>What's For Dinner?</h1>
+                    {/* <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/event/:">Event Page</Link></li>
+                        </ul>
+                    </nav> */}
+                </header>
+                <section className="start">
+                    <div className="wrapper startContainer">
+                        <div className="tagLine">
+                            <h2>We help you organize Dinner Parties</h2>
+                        </div>
+                        <div className="startForm">
+                            <form 
+                            // onSubmit={this.pushToFirebase} 
+                            onSubmit={this.props.createEvent}
+                            action=""> 
+                                <input onChange={this.props.getEventName} name="createEvent" className="createEvent" type="text" placeholder="enter your group name"/>
+                                <label htmlFor="createEvent" class="visuallyHidden">Enter your group name</label>
+                                <button className="submit">
+                                    Submit
+                                    {/* This button creates a new event object in the events array AND links to event page. */}
+                                </button>      
+                            </form>
+                            <p>or</p>
+                            <button className="skipToEvents"
+                            onClick={this.moveToSection}>
+                                Skip
+                                {/* link to events (scroll) */}
+                            </button>
+                        </div>
                     </div>
-                    <div className="startForm">
-                        <form 
-                        // onSubmit={this.pushToFirebase} 
-                        onSubmit={props.createEvent}
-                        action=""> 
-                            <input onChange={props.getEventName} name="createEvent" className="createEvent" type="text" placeholder="enter your group name"/>
-                            <label htmlFor="createEvent" class="visuallyHidden">Enter your group name</label>
-                            <button className="submit">
-                                Submit
-                                {/* This button creates a new event object in the events array AND links to event page. */}
-                            </button>      
-                        </form>
-                        <p>or</p>
-                        <button className="skipToEvents">
-                            Skip
-                            {/* link to events (scroll) */}
-                        </button>
+                </section>
+                <section className="events" name="events">
+                    <div className="wrapper">
+                        <div>
+                            <h2>Events</h2>
+                            <h3>Find your dinner party</h3>
+                        </div>
+                        <ul>
+                            {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
+                            {/* {this.props["event"] ?console.logthis.(props["event"][0]):console.log('null')} */}
+                            {this.props.event?
+                                this.props.event.map((userEvents, eventIndex) => {
+                                    return (
+                                        // console.log(userEvents.eventName)
+                                        <li>
+                                            <Link to={`/dashboard/${userEvents.eventName}`} 
+                                            key={eventIndex}>{userEvents.eventName}</Link>
+                                        </li>
+                                    )
+                                }
+                            ):null}
+                        </ul>
                     </div>
-                </div>
-            </section>
-            <section className="events">
-                <div className="wrapper">
-                    <div>
-                        <h2>Events</h2>
-                        <h3>Find your dinner party</h3>
-                    </div>
-                    <ul>
-                        {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
-                        {/* {props["event"] ?console.log(props["event"][0]):console.log('null')} */}
-                        {props.event?
-                            props.event.map((userEvents, eventIndex) => {
-                                return (
-                                    // console.log(userEvents.eventName)
-                                    <li>
-                                        <Link to={`/dashboard/${userEvents.eventName}`} 
-                                        key={eventIndex}>{userEvents.eventName}</Link>
-                                    </li>
-                                )
-                            }
-                        ):null}
-                    </ul>
-                </div>
-            </section>
-        </div>
-    
-    );
+                </section>
+            </div>
+        
+        );
+    }
   
 }
 export default LandingPage;
