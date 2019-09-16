a0529e7a05a180bb9be7dc92a484d195541a2069
Seth Poulin <seth.poulin@gmail.com>
2019-09-16 17:28:03 -0400

    fixed duplicate event problem.  Still need to clear error when user starts to enter new event name.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,91 +7,96 @@ import firebase from '../firebase';
 
 
 
-const LandingPage = (props) => {
+class LandingPage extends Component {
+    constructor() {
+        super() 
+        this.state = {
+            userIsDuplicate: false,
+        }
+    }
     
-    const checkName = (e) => {
+    checkName = (e) => {
         e.preventDefault();
-        const message = () => <p>:Telkj;</p>;
+        const message = () => this.setState
         const eventNames = [];
-        console.log('checkname was fired');
     
-        (props.event).forEach((object)=> {
+        (this.props.event).forEach((object)=> {
             eventNames.push(object.eventName);
         });
-        (eventNames.includes(`${props.createEventName}`))? message() : props.createEvent();
+        // returns true if there is a duplicate
+        eventNames.includes(`${this.props.createEventName}`)? this.setState({
+            userIsDuplicate: !this.state.userIsDuplicate
+        }) : this.props.createEvent();
         //forEach loop over props.event array and pull out each object.name value to create an array of eventNames
         //if eventNames.includes(`${props.createEventName}`) then
         //return true else return false
     }
     
   
-    return (
+    render(){
+        
+        return (
     
-        <div className="landingPage">
-            <header className="landingHeader">
-                <h1>What's For Dinner?</h1>
+            <div className="landingPage">
+                <header className="landingHeader">
+                    <h1>What's For Dinner?</h1>
 
-            </header>
-            <section className="start">
-                <div className="wrapper startContainer">
-                    <div className="tagLine">
-                        <h2>We help you organize Dinner Parties</h2>
-                    </div>
-                    <div className="startForm">
-                        <form 
-                        // onSubmit={this.pushToFirebase} 
-                        onSubmit={
-                            // console.log('I was clicked')
-                            checkName
-                            // ? props.createEvent : console.log('this is a duplicate event')
-                            
-                            // <p>An event with that name already exists.  Please select a different name.</p>
+                </header>
+                <section className="start">
+                    <div className="wrapper startContainer">
+                        <div className="tagLine">
+                            <h2>We help you organize Dinner Parties</h2>
+                        </div>
+                        <div className="startForm">
+                            <form 
+                            // onSubmit={this.pushToFirebase} 
+                            onSubmit={this.checkName}
+                            action=""> 
+                                {this.props.errorMessage !=='' ? <p>{this.props.errorMessage}</p> : null}
+                                <input onChange={this.props.getEventName} name="createEvent" className="createEvent" type="text" placeholder="enter your group name"/>
+                                <label htmlFor="createEvent" className="visuallyHidden">Enter your group name</label>
+                                <button className="submit">
+                                    Submit
+                                    {/* This button creates a new event object in the events array AND links to event page. */}
+                                </button>      
+                            </form>
+                            {/* print error message if state is true */}
+                            <p>or</p>
+                            <button className="skipToEvents">
+                                Skip
+                                {/* link to events (scroll) */}
+                            </button>
+                            { this.state.userIsDuplicate? <p>User already exists</p> : null}
 
-                            // run checkfunction.  if checkfunction is negative, run props.createEvent, otherwise return a message.
-                        }
-                        action=""> 
-                            {props.errorMessage !=='' ? <p>{props.errorMessage}</p> : null}
-                            <input onChange={props.getEventName} name="createEvent" className="createEvent" type="text" placeholder="enter your group name"/>
-                            <label htmlFor="createEvent" className="visuallyHidden">Enter your group name</label>
-                            <button className="submit">
-                                Submit
-                                {/* This button creates a new event object in the events array AND links to event page. */}
-                            </button>      
-                        </form>
-                        <p>or</p>
-                        <button className="skipToEvents">
-                            Skip
-                            {/* link to events (scroll) */}
-                        </button>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section className="events">
-                <div className="wrapper">
-                    <div>
-                        <h2>Events</h2>
-                        <h3>Find your dinner party</h3>
+                </section>
+                <section className="events">
+                    <div className="wrapper">
+                        <div>
+                            <h2>Events</h2>
+                            <h3>Find your dinner party</h3>
+                        </div>
+                        <ul>
+                            {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
+                            {/* {props["event"] ?console.log(props["event"][0]):console.log('null')} */}
+                            {this.props.event?
+                                this.props.event.map((userEvents, eventIndex) => {
+                                    return (
+                                        // console.log(userEvents.eventName)
+                                        <li key={eventIndex} >
+                                            <Link to={`/dashboard/${userEvents.eventName}`} 
+                                            >{userEvents.eventName}</Link>
+                                        </li>
+                                    )
+                                }
+                            ):null}
+                        </ul>
                     </div>
-                    <ul>
-                        {/* map through this.state.events and return events to page as <li> elements in <Link>s. */}
-                        {/* {props["event"] ?console.log(props["event"][0]):console.log('null')} */}
-                        {props.event?
-                            props.event.map((userEvents, eventIndex) => {
-                                return (
-                                    // console.log(userEvents.eventName)
-                                    <li key={eventIndex} >
-                                        <Link to={`/dashboard/${userEvents.eventName}`} 
-                                        >{userEvents.eventName}</Link>
-                                    </li>
-                                )
-                            }
-                        ):null}
-                    </ul>
-                </div>
-            </section>
-        </div>
-    
-    );
+                </section>
+            </div>
+        )
+   };  
   
 }
 export default LandingPage;
